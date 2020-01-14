ac3e2a0843921d56ca556f59bf38b06d34f45480
James Finch <jdfinch@emory.edu>
2020-01-14 01:53:46 -0500

    updated pip package

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 setuptools.setup(
      name='emora-stdm',
-     version='1.0',
+     version='0.9',
      scripts=[],
      author="James Finch",
      author_email="jdfinch@emory.edu",
