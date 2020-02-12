b31f80796e7ea37513d36867545ac0ac9d3f84b6
James Finch <jdfinch@emory.edu>
2020-02-11 23:27:04 -0500

    trying to pull

--- emora_stdm/modules/family.py
+++ emora_stdm/modules/family.py
@@ -2,13 +2,31 @@
 from emora_stdm import DialogueFlow, NatexNLU, NatexNLG, Macro
 
 
-df = DialogueFlow(initial_state="start", kb='family.json')
+df = DialogueFlow(initial_state="start", kb='family.json', initial_speaker=DialogueFlow.Speaker.USER)
 
-# living with
+
+df.add_state('start', error_successor='start')
+df.add_system_transition('start', 'start', 'ok then')
+
 df.add_state('living with prompt')
+df.add_state('valentines prompt')
+
+df.add_user_transition('start', 'valentines prompt', '[{valentines, valentine}]')
+df.add_user_transition('start', 'living with prompt', '[{my house, at home, go home, my apartment, my home}]')
+
 df.add_state('living with response', error_successor='living with unsure')
 
 df.add_state('living with unsure')
+
+df.add_state('valentines response')
+df.add_state('valentines person')
+df.add_state('valentines plans')
+df.add_state('valentines nobody')
+df.add_state('valentines broke up')
+df.add_state('valentines appreciation')
+df.add_state('valentines broke up reaction')
+df.add_state('valentines person reaction')
+df.add_state('valentines plans reaction')
 
 df.add_state('living with spouse')
 df.add_state('living with roommate')
@@ -54,6 +72,30 @@ df.add_state('end', error_successor='end')
 
 df.add_system_transition('living with prompt', 'living with response',
                          '"so, who do you live with?"')
+df.add_system_transition('valentines prompt', 'valentines response',
+                         '"Yeah. Valentines day is coming. Do you have any plans?"')
+df.add_user_transition('valentines response', 'valentines person',
+                         "[!#ONT_NEG(negation), -{wish, want} [$valentine={#ONT(partner)}]]", score=2.0)
+df.add_user_transition('valentines response', 'valentines broke up',
+                       '[{divorce, divorced, broke up, split up, left me}]', score=2.0)
+df.set_error_successor('valentines response', 'valentines appreciation')
+df.add_user_transition('valentines response', 'valentines yes',
+                       '[#ONT_NEG(negation), {yes, yeah, yep, yup, yea, sure}]')
+df.add_system_transition('valentines yes', 'valentines response', '"Thats exciting! What are you going to do?"')
+df.add_user_transition('valentines response', 'valentines plans', '[$activity={dinner, date}]')
+df.add_system_transition('valentines plans', 'valentines plans reaction',
+                         '[!a, $activity, sounds like a lot of, "fun. Hope you have a good time."]')
+df.set_error_successor('valentines plans reaction', 'valentines appreciation')
+
+df.add_system_transition('valentines broke up', 'valentines broke up reaction',
+                         '"Oh. I am really sorry to hear that. Being apart from someone who used to be in your life can be hard."')
+df.add_system_transition('valentines person', 'valentines person reaction',
+                         '[!Well I hope you and your $valentine have a good time]')
+df.set_error_successor('valentines broke up reaction', 'valentines appreciation')
+df.set_error_successor('valentines person reaction', 'valentines appreciation')
+
+df.add_system_transition('valentines appreciation', 'end',
+                         "I think valentines day is just a good time to let someone know you appreciate them. If you can just do that, you will make someone very happy.")
 
 df.add_system_transition('living with unsure', 'living with response', '"I didnt catch that, who did you say you live with?"')
 
@@ -72,7 +114,7 @@ df.add_user_transition('living with response', 'living alone',
                            '[{alone, by myself, solo, nobody, noone, no one}]',
                            '[!#NOT(negation), [{living, live, with, #EXP(anyone)}]]'
                        ])
-df.add_system_transition('living with spouse', 'personality', '[!what is your $partnertype "like?"]')
+df.add_system_transition('living with spouse', 'personality', '[!what is your $livingwith "like?"]')
 df.add_system_transition('living with roommate', 'personality', '[!what is your $livingwith "like?"]')
 df.add_system_transition('living with friend', 'personality', '[!what is your $livingwith "like?"]')
 df.add_system_transition('living with parents', 'personality', '[!what is your $livingwith "like?"]')
@@ -97,6 +139,7 @@ df.add_user_transition('personality', 'annoying', '[annoying]')
 df.add_user_transition('personality', 'hard working', '[hard working]')
 df.add_user_transition('personality', 'fun', '[{fun, carefree}]')
 df.add_user_transition('personality', 'worrying', '[{worries, worrying, worrisome}]')
+df.add_user_transition('personality', 'beautiful', '[{beautiful, pretty, gorgeous, hot, sexy}]')
 
 df.add_system_transition('smart', 'end', '"I wish I was smart"')
 df.add_system_transition('funny', 'end', '"Its good to live with a sense of humor"')
@@ -111,11 +154,12 @@ df.add_system_transition('annoying', 'end', '"Oh no, I hope you keep your sanity
 df.add_system_transition('hard working', 'end', '"thats admirable but sometimes it can be good to take life slow too"')
 df.add_system_transition('fun', 'end', '"Thats great, spending time with people who are fun like that is great"')
 df.add_system_transition('worrying', 'end', '"I see. I think its hard to be stressed all the time."')
+df.add_system_transition('beautiful', 'end', '"Aww. Its nice you are with someone that you find beauty in."')
 
-df.add_system_transition('end', 'end', '"okay then"')
+df.add_system_transition('end', 'end', '"ok then"')
 
 if __name__ == '__main__':
-    df.set_state('living with prompt')
+    df.set_state('start')
     df.run(debugging=True)
 
 
--- emora_stdm/modules/family_loop.py
+++ emora_stdm/modules/family_loop.py
@@ -2,12 +2,18 @@
 from emora_stdm import DialogueFlow, NatexNLU, NatexNLG, Macro
 
 
-df = DialogueFlow(initial_state="start", kb='family.json')
+df = DialogueFlow(initial_state="root", kb='family.json')
 
-df.add_state('root')
+df.add_state('root', error_successor='root')
+df.add_system_transition('root', 'root', '{yeah, for sure, okay}', score=0)
 
-df.add_state('living with prompt')
-df.add_state('living with reaction')
-df.add_state('personality')
-df.add_state('personality reaction')
+# related
+df.add_state('related', 'root', user_multi_hop=True)
+df.add_user_transition('root', 'related',
+                       [
+                           '[!my $related_type=#ONTE(partner)]',
+                           '[!my $related_type=#ONTE(parent)]'
+                       ])
 
+if __name__ == '__main__':
+    df.run(debugging=True)
