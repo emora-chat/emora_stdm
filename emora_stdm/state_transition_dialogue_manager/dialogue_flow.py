8c4eb39ec791b3d2ff5e9e3aa3db0cda616de896
James Finch <jdfinch@emory.edu>
2020-02-11 23:29:39 -0500

    destroyed print statement

--- emora_stdm/state_transition_dialogue_manager/dialogue_flow.py
+++ emora_stdm/state_transition_dialogue_manager/dialogue_flow.py
@@ -86,8 +86,6 @@ class DialogueFlow:
                 user_input = input("U: ")
                 self.user_turn(user_input, debugging=debugging)
 
-            print(self.state_settings(self.state()).memory._data)
-
     def system_turn(self, debugging=False):
         """
         an entire system turn comprising a single system utterance and
