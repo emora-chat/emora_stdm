315f7e9aebe2205fe556fec4e8d5da376653f5af
Sarah Fillwock <sarah.fillwock@emory.edu>
2020-02-12 14:50:31 -0500

    global transitions added into all components, topic suggestions added, repetition avoided

--- emora_stdm/modules/dialogue_manager.py
+++ emora_stdm/modules/dialogue_manager.py
@@ -11,52 +11,61 @@ from modules.stop_conversation import df as stop
 from modules.stop_conversation import State as stop_states
 from modules.stop_conversation import stop_nlu
 
+def add_global_nlu_to_components(components, state, nlu, knowledge_file=None):
+    for name,component in components.items():
+        if knowledge_file is not None:
+            component._kb.load_json_file(knowledge_file)
+        if component.has_state(state):
+            component.update_state_settings(state, global_nlu=nlu)
+        else:
+            component.add_state(state, global_nlu=nlu)
+
 class Available(Macro):
     def run(self, ngrams, vars, args):
-        if 'dm-not_available' in vars:
-            if args[0] not in vars['dm-not_available']:
+        if 'dm_not_available' in vars:
+            if args[0] not in vars['dm_not_available']:
                 return True
             return False
         return True
 
 class NotAvailable(Macro):
     def run(self, ngrams, vars, args):
-        if 'dm-not_available' in vars:
+        if 'dm_not_available' in vars:
             for arg in args:
-                if arg not in vars['dm-not_available']:
+                if arg not in vars['dm_not_available']:
                     return False
             return True
         return False
 
 class UpdateNotAvailable(Macro):
     def run(self, ngrams, vars, args):
-        if 'dm-not_available' not in vars:
-            vars['dm-not_available'] = set()
-        vars['dm-not_available'].add(args[0])
+        if 'dm_not_available' not in vars:
+            vars['dm_not_available'] = set()
+        vars['dm_not_available'].add(args[0])
 
 class GetTopicSuggestions(Macro):
     def run(self, ngrams, vars, args):
-        options = vars['dm-available']
-        if 'dm-not_available' in vars:
-            options = options - vars['dm-not_available']
+        options = vars['dm_available']
+        if 'dm_not_available' in vars:
+            options = options - vars['dm_not_available']
         if len(options) > 0:
-            vars['dm-suggested_topic'] = random.choice(tuple(options))
-            return vars['dm-suggested_topic']
+            vars['dm_suggested_topic'] = random.choice(tuple(options))
+            return vars['dm_suggested_topic']
         else:
             return False
 
 class IsTopicSuggestion(Macro):
     def run(self, ngrams, vars, args):
-        if 'dm-suggested_topic' in vars and args[0] == vars["dm-suggested_topic"]:
+        if 'dm_suggested_topic' in vars and args[0] == vars["dm_suggested_topic"]:
             return True
         return False
 
 macros = {"Available":Available(), "NotAvailable":NotAvailable(), "UpdateNotAvailable":UpdateNotAvailable(),
           "GetTopicSuggestions":GetTopicSuggestions(), "IsTopicSuggestion":IsTopicSuggestion()}
 
-cdf = CompositeDialogueFlow('start', initial_speaker=DialogueFlow.Speaker.USER, macros=macros, kb=os.path.join('modules',"stop_convo.json"))
+cdf = CompositeDialogueFlow('start', initial_speaker=DialogueFlow.Speaker.USER, macros=macros)
 cdf.controller()._kb.load_json_file(os.path.join('modules',"hobbies.json"))
-cdf.set_vars({'dm-available': {'movies','music','sports','news','pet','hobby'}})
+cdf.set_vars({'dm_available': {'movies','music','sports','news','pet','hobby'}})
 cdf.add_component(opening, 'opening')
 cdf.add_user_transition('start', ('opening', 'prestart'), '/.*/')
 cdf.controller().update_state_settings(('opening', 'prestart'), user_multi_hop=True)
@@ -92,7 +101,7 @@ pet.update_state_settings(('SYSTEM', 'intermediate_topic_switch'), system_multi_
 # family.update_state_settings(('SYSTEM', 'topic_master'), system_multi_hop=True)
 
 cdf.add_component(stop, 'stop')
-cdf.controller().add_state(('stop', stop_states.REC_OFF), global_nlu=stop_nlu)
+add_global_nlu_to_components(cdf._components, ('stop', stop_states.REC_OFF), stop_nlu, os.path.join('modules', "stop_convo.json"))
 stop.add_system_transition(stop_states.END, ('SYSTEM', 'topic_master'), '"."')
 
 
@@ -100,12 +109,20 @@ stop.add_system_transition(stop_states.END, ('SYSTEM', 'topic_master'), '"."')
 # External components
 ########################
 
-cdf.controller().add_state('movies', global_nlu='[{movie,movies,film,films,tv,shows,television}]')
-cdf.controller().add_state('music', global_nlu='[{music,song,songs,melody,melodies,album,albums,concert,concerts}]')
-cdf.controller().add_state('sports', global_nlu='[{sport,sports,athletics,basketball,football,hockey,golf,tennis}]')
-cdf.controller().add_state('news', global_nlu='[{news}]')
-cdf.controller().update_state_settings(('pet', pet_states.PETS_Y), global_nlu='[{pet,pets,animals,animal,cat,cats,dog,dogs}]')
-cdf.controller().update_state_settings(('hobby', hobby_states.INTRO_READING), global_nlu='[{hobby,hobbies,activity,activities,fun things,things to do,pasttimes}]')
+add_global_nlu_to_components(cdf._components, ('SYSTEM','movies'), '[{movie,movies,film,films,tv,shows,television}]')
+add_global_nlu_to_components(cdf._components, ('SYSTEM','music'), '[{music,song,songs,melody,melodies,album,albums,concert,concerts}]')
+add_global_nlu_to_components(cdf._components, ('SYSTEM','sports'), '[{sport,sports,athletics,basketball,football,hockey,golf,tennis}]')
+add_global_nlu_to_components(cdf._components, ('SYSTEM','news'), '[{news}]')
+add_global_nlu_to_components(cdf._components, ('pet', pet_states.PETS_Y), '[{pet,pets,animals,animal,cat,cats,dog,dogs}]')
+add_global_nlu_to_components(cdf._components, ('hobby', hobby_states.INTRO_READING), '[{hobby,hobbies,activity,activities,fun things,things to do,pasttimes}]')
+
+
+# cdf.controller().add_state('movies', global_nlu='[{movie,movies,film,films,tv,shows,television}]')
+# cdf.controller().add_state('music', global_nlu='[{music,song,songs,melody,melodies,album,albums,concert,concerts}]')
+# cdf.controller().add_state('sports', global_nlu='[{sport,sports,athletics,basketball,football,hockey,golf,tennis}]')
+# cdf.controller().add_state('news', global_nlu='[{news}]')
+# cdf.controller().update_state_settings(('pet', pet_states.PETS_Y), global_nlu='[{pet,pets,animals,animal,cat,cats,dog,dogs}]')
+# cdf.controller().update_state_settings(('hobby', hobby_states.INTRO_READING), global_nlu='[{hobby,hobbies,activity,activities,fun things,things to do,pasttimes}]')
 
 cdf.controller().add_system_transition('movies', 'topic_master', '[! #UpdateNotAvailable(movies) "Movie stub."]')
 cdf.controller().update_state_settings('movies', system_multi_hop=True)
@@ -136,7 +153,7 @@ cdf.add_system_transition('accept_topic', 'music', '[! #IsTopicSuggestion(music)
 cdf.add_system_transition('accept_topic', 'sports', '[! #IsTopicSuggestion(sports) "."]')
 cdf.add_system_transition('accept_topic', 'news', '[! #IsTopicSuggestion(news) "."]')
 cdf.controller().update_state_settings('accept_topic', memory=0)
-cdf.add_system_transition('reject_topic', 'topic_master', '"Ok, of course. Let\'s find something else to talk about."')
+cdf.add_system_transition('reject_topic', 'topic_master', '[! "Ok, of course. Let\'s find something else to talk about." #UpdateNotAvailable($dm_suggested_topic)]')
 
 cdf.add_system_transition('suggest_topic', 'no_topic', '"We have covered a lot of topics so far. What else would you like to talk about now?"', score=0.0)
 cdf.add_system_transition('catchall', 'catchall', '"Interesting! Tell me more."')
