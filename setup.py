45c1bbec26e6e2cebb44707c33d5238afe2b8997
Sarah Fillwock <sfillwo@emory.edu>
2020-03-16 22:49:18 -0400

    updated setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 setuptools.setup(
      name='emora-stdm',
-     version='1.32',
+     version='1.35',
      scripts=[],
      author="James Finch",
      author_email="jdfinch@emory.edu",
@@ -13,7 +13,7 @@ setuptools.setup(
      url="https://github.com/emora-chat/EmoraSTDM.git",
      packages=setuptools.find_packages(),
      install_requires=[
-         "structpy==0.2",
+         "structpy==0.3",
          "lark-parser",
          "regex",
          "nltk",
