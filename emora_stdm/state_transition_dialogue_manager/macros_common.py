f79e0dea544f2025cd866eaa7b7887a486984b26
Sarah Fillwock <sfillwo@emory.edu>
2020-05-20 02:09:58 -0400

    resolved SBS bug, removed debugging prints

--- emora_stdm/state_transition_dialogue_manager/macros_common.py
+++ emora_stdm/state_transition_dialogue_manager/macros_common.py
@@ -563,7 +563,6 @@ class ScoreBySimilarity(Macro):
         match = nlp(', '.join(args[1:]))
         value = nlp(vars[variable])
         similarity = Intent._similarity(None, match, value)
-        print(value, "<->", match, "=", similarity)
         vars['__score__'] = similarity
         if similarity < threshold:
             return False
