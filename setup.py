43928260359b8215cd45a7812e55759ce253b1fe
Sarah Fillwock <sfillwo@emory.edu>
2020-06-03 22:35:06 -0400

    v1.90

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 setuptools.setup(
      name='emora-stdm',
-     version='1.89',
+     version='1.90',
      scripts=[],
      author="James Finch",
      author_email="jdfinch@emory.edu",
