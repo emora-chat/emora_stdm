d34e887ecaf01bf38b5ae01427081bfe70d95739
jdfinch <jdfinch@emory.edu>
2022-08-23 11:42:08 -0400

    version increment to push pip

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 setuptools.setup(
      name='emora-stdm',
-     version='1.97.3',
+     version='2.0.0',
      scripts=[],
      author="James Finch",
      author_email="jdfinch@emory.edu",
